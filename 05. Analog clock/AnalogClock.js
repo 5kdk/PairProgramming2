e7a3200e35486443711adc151d2680c3ee2a3bcf
5kdk <tylerk1121@gmail.com>
2023-02-22 21:55:38 +0900

    refactor: Update query selector to variable
    Co-authored-by: Kyusung Sohn <rok.ksohn@gmail.com>

--- 05. Analog clock/AnalogClock.js
+++ 05. Analog clock/AnalogClock.js
@@ -1,4 +1,5 @@
 // do something!
+
 const AnalogClock = $container => {
   $container.innerHTML = `
     <div class="hand hour"></div>
@@ -18,17 +19,20 @@ const AnalogClock = $container => {
     <div class="time time12">|</div>
   `;
 
+  const $hands = [...$container.querySelectorAll('.hand')];
+
   const tick = () => {
     const currentTime = new Date();
     const [hr, min, sec] = [currentTime.getHours(), currentTime.getMinutes(), currentTime.getSeconds()];
+    const [$hour, $minute, $second] = $hands;
 
     const secDegree = sec / 60;
     const minDegree = (secDegree + min) / 60;
     const hrDegree = (minDegree + hr) / 12;
 
-    $container.querySelector('.hand.hour').style.setProperty('--deg', hrDegree * 360);
-    $container.querySelector('.hand.minute').style.setProperty('--deg', minDegree * 360);
-    $container.querySelector('.hand.second').style.setProperty('--deg', secDegree * 360);
+    $hour.style.setProperty('--deg', hrDegree * 360);
+    $minute.style.setProperty('--deg', minDegree * 360);
+    $second.style.setProperty('--deg', secDegree * 360);
   };
 
   tick();
