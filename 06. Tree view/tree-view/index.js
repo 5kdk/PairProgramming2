abb4a6c19993887ef3982b4e368efc766c0310c0
5kdk <tylerk1121@gmail.com>
2023-02-22 17:30:11 +0900

    feat: Add TreeView class
    Co-authored-by: Kyusung Sohn <rok.ksohn@gmail.com>

--- 06. Tree view/tree-view/index.js
+++ 06. Tree view/tree-view/index.js
@@ -1,5 +1,41 @@
 class TreeView {
   /* do something! */
+  constructor($container, tree) {
+    this.$container = $container;
+    this.tree = tree;
+
+    window.addEventListener('DOMContentLoaded', () => {
+      this.initialize();
+    });
+  }
+
+  initialize() {
+    this.$container.innerHTML = `
+    <ul class="tree-container">
+    ${this.tree.map(item => this.createDOMString(item)).join('')}
+    </ul>`;
+  }
+
+  // prettier-ignore
+  createDOMString({ name, children, isOpen }) {
+    return `
+      <li class="tree-node">
+        <a href="#" data-name="${name}" data-event-type="${ !children ? 'select' : isOpen ? 'expand' : 'collapse'}">
+          <span class="tree-switcher ${ !children ? 'noop' : isOpen ? 'expand' : 'collapse'}"></span>
+          <span class="tree-content">${name}</span>
+        </a>
+        ${children ? children.map(item => `
+        <ul class="subtree-container ${isOpen ? '' : 'hide'}">
+          ${this.createDOMString(item)}
+        </ul>`).join('') : ''}
+      </li>`
+  }
+
+  on(eventType, e) {
+    if (eventType === 'collapse') this.handleCollapse(e);
+    if (eventType === 'select') this.handleSelect(e);
+    if (eventType === 'expand') this.handleExpand(e);
+  }
 }
 
 export default TreeView;
