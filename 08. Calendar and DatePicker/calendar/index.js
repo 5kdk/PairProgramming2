c3812917b5b1af3bc2f1b6dddbcef25849e9795e
5kdk <tylerk1121@gmail.com>
2023-02-24 21:49:15 +0900

    refactor: Update Calendar, DatePicker functions v2
    Co-authored-by: Kyusung Sohn <rok.ksohn@gmail.com>

--- 08. Calendar and DatePicker/calendar/index.js
+++ 08. Calendar and DatePicker/calendar/index.js
@@ -2,70 +2,99 @@
 const MONTHS = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December',];
 const WEEKDAYS = ['SUN', 'MON', 'TUE', 'WED', 'THU', 'FRI', 'SAT'];
 
+// ---------------------- calendar function --------------------
 const Calendar = $container => {
   let state = {
     year: new Date().getFullYear(),
     month: new Date().getMonth(),
-    day: new Date().getDate(),
+  };
+
+  const getEmptyCounts = () => {
+    const prevEmptyCount = new Date(state.year, state.month, 1).getDay();
+    const nextEmptyCount = 7 - (new Date(state.year, state.month + 1, 0).getDay() + 1);
+
+    return [prevEmptyCount, nextEmptyCount];
+  };
+
+  const getFullCalendar = () => {
+    const [prevEmptyCount, nextEmptyCount] = getEmptyCounts();
+
+    const prevLastDay = new Date(state.year, state.month, 0).getDate();
+
+    const prevMonth = new Array(prevEmptyCount).fill(prevLastDay - prevEmptyCount + 1).map((ele, idx) => ele + idx);
+    const month = new Array(new Date(state.year, state.month + 1, 0).getDate()).fill('').map((_, idx) => idx + 1);
+    const nextMonth = new Array(nextEmptyCount).fill('').map((_, idx) => idx + 1);
+
+    return [prevMonth, month, nextMonth];
   };
 
   const isToday = (year, month, day) =>
     year === new Date().getFullYear() && month === new Date().getMonth() && day === new Date().getDate();
 
-  $container.innerHTML += `<div class="calendar"></div>`;
+  // prettier-ignore
+  const renderCalendar = () => {
+    const [prevEmptyCount] = getEmptyCounts();
+    const [prevMonth, current, nextMonth] = getFullCalendar();
+    const $days = [...prevMonth, ...current, ...nextMonth];
 
-  const render = () => {
-    const { year, month } = state;
+    $container.querySelector('.calendar').innerHTML = `
+      <nav class="calendar-nav">
+        <button class="prev">◀</button>
+        <div>
+          <p class="current-month">${MONTHS[state.month]}</p>
+          <p class="current-year">${state.year}</p>
+        </div>
+        <button class="next">▶</button>
+      </nav>
+      <section class="calendar-grid">
+        ${WEEKDAYS.map(weekday => `<div class="day">${weekday}</div>`).join('')}
+        ${$days.map((day, idx) => idx >= prevEmptyCount && idx < prevEmptyCount + current.length
+          ? `<div class="day active ${isToday(state.year, state.month, day) ? 'today': ''} ${idx % 7 === 0 ? 'sunday' : ''}">${day}</div>`
+          : `<div class="day">${day}</div>`).join('')}
+      </section>`
+    };
 
-    const prevCount = new Date(year, month, 1).getDay();
-    const nextCount = 7 - (new Date(year, month + 1, 0).getDay() + 1);
-    const prevDate = new Date(year, month, 0).getDate();
-
-    const prev = new Array(prevCount).fill(prevDate - prevCount + 1).map((ele, idx) => ele + idx);
-    const cur = new Array(new Date(state.year, state.month + 1, 0).getDate()).fill('').map((_, idx) => idx + 1);
-    const next = new Array(nextCount).fill('').map((_, idx) => idx + 1);
-
-    const dayData = [...prev, ...cur, ...next];
-
-    // prettier-ignore
-    document.querySelector('.calendar').innerHTML = `
-      <div class="calendar">
-        <nav class="calendar-nav">
-          <button class="prev">◀</button>
-          <div>
-            <p class="current-month">${MONTHS[state.month]}</p>
-            <p class="current-year">${state.year}</p>
-          </div>
-          <button class="next">▶</button>
-        </nav>
-        <section class="calendar-grid">
-          ${WEEKDAYS.map(weekday => `<div class="day">${weekday}</div>`).join('')}
-          ${dayData.map((day, idx) => idx >= prevCount && idx < prevCount + cur.length
-            ? `<div class="day active ${isToday(state.year, state.month, day) ? 'today': ''} ${idx % 7 === 0 ? 'sunday' : ''}">${day}</div>`
-            : `<div class="day">${day}</div>`).join('')}
-        </section>
-      </div>`
-  };
   const setState = newState => {
     state = { ...state, ...newState };
-    render();
+    renderCalendar();
   };
 
-  document.addEventListener('DOMContentLoaded', setState);
+  const initialize = () => {
+    $container.innerHTML += `<div class="calendar"></div>`;
+    renderCalendar();
+  };
 
-  $container.addEventListener('click', e => {
+  const handleCalendar = e => {
     if (!e.target.matches('.active, .prev, .next')) return;
 
-    if (e.target.classList.contains('prev'))
+    const goToPrevMonth = () => {
       setState(state.month === 0 ? { year: state.year - 1, month: 11 } : { month: state.month - 1 });
-    if (e.target.classList.contains('next'))
-      setState(state.month === 11 ? { year: state.year + 1, month: 0 } : { month: state.month + 1 });
-    if (e.target.classList.contains('day')) {
-      const clickedDate = `${state.year}-${(state.month + 1 + '').padStart(2, 0)}-${e.target.textContent}`;
+    };
 
-      console.log(clickedDate);
-      document.querySelector('.date-picker-input').value = clickedDate;
-    }
+    const goToNextMonth = () => {
+      setState(state.month === 11 ? { year: state.year + 1, month: 0 } : { month: state.month + 1 });
+    };
+
+    // prettier-ignore
+    const handleDayClick = () => {
+      const clickedDate = `${state.year}-${(state.month + 1 + '').padStart(2, 0)}-${e.target.textContent.padStart(2, 0)}`;
+
+      $container.children[1].value = clickedDate;
+    };
+
+    if (e.target.matches('.prev')) goToPrevMonth();
+    if (e.target.matches('.next')) goToNextMonth();
+    if (e.target.matches('.active')) handleDayClick();
+  };
+
+  document.addEventListener('DOMContentLoaded', initialize);
+
+  $container.addEventListener('click', handleCalendar);
+  $container.addEventListener('calendar-hidden', e => {
+    setState({
+      year: new Date(e.detail.selectedDate).getFullYear(),
+      month: new Date(e.detail.selectedDate).getMonth(),
+    });
   });
 };
 
--- 08. Calendar and DatePicker/date-picker/index.js
+++ 08. Calendar and DatePicker/date-picker/index.js
@@ -1,8 +1,19 @@
 const DatePicker = $container => {
   $container.innerHTML = `
     <h1>Date Picker</h1>
-    <input type="text" class="date-picker-input" readonly>
+    <input class="date-picker-input" placeholder="Select a date" readonly>
   `;
+
+  $container.addEventListener('click', e => {
+    if (!e.target.matches('.date-picker-input')) return;
+
+    $container.querySelector('.calendar').classList.toggle('hidden');
+
+    if (!e.target.value) return;
+
+    const customEvent = new CustomEvent('calendar-hidden', { detail: { selectedDate: e.target.value } });
+    $container.dispatchEvent(customEvent);
+  });
 };
 
 export default DatePicker;
