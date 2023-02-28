95fd6286452e36045fdd59a455c4ea00b6017c71
Kyu Sung Sohn <rok.ksohn@gmail.com>
2023-02-28 10:09:41 +0900

    refactor: Update mission09 all functions
    Co-authored-by: Dongkyu Kim <5kdk@users.noreply.github.com>

--- 09. News Viewer/components/NewsList.js
+++ 09. News Viewer/components/NewsList.js
@@ -1,82 +1,87 @@
+/* eslint-disable no-undef */
 // do something!
 const NewsList = $root => {
-  const API_KEY = '3fb91bc39b5a462f93b8e29ae16f03d3';
+  const API_KEY = '5d62f4885aa74eb2a6674c56d41200d2';
   const MAX_PAGE = 5;
 
   let category = '';
   let page = 1;
 
+  // prettier-ignore
+  const initialize = () => {
+    $root.innerHTML += `
+      <div class="news-list-container">
+        <div class="scroll-observer">
+          <img src="img/ball-triangle.svg" alt="Loading..." />
+        </div>
+      </div>`;
+  };
+
+  // prettier-ignore
   const render = posts => {
-    if (!document.querySelector('.news-list-container')) {
-      $root.innerHTML += `
-        <div class="news-list-container">
-          <div class="scroll-observer">
-            <img src="img/ball-triangle.svg" alt="Loading..." />
+    const $observer = document.querySelector('.scroll-observer');
+    const domStr = posts.map(post => `
+      <article class="news-list">
+        <section class="news-item">
+          <div class="thumbnail">
+            <a href="${post.url}" target="_blank" rel="noopener noreferrer">
+              <img src="${post.urlToImage}" alt="thumbnail" />
+            </a>
           </div>
-        </div>`;
-    }
-
-    posts.forEach(post => {
-      const $article = document.createElement('article');
-      $article.classList.add('news-list');
-
-      $article.innerHTML = `
-        <article class="news-list">
-          <section class="news-item">
-            <div class="thumbnail">
+          <div class="contents">
+            <h2>
               <a href="${post.url}" target="_blank" rel="noopener noreferrer">
-                <img src="${post.urlToImage}" alt="thumbnail" />
+                ${post.title}
               </a>
-            </div>
-            <div class="contents">
-              <h2>
-                <a href="${post.url}" target="_blank" rel="noopener noreferrer">
-                  ${post.title}
-                </a>
-              </h2>
-              <p>
-                ${post.description}
-              </p>
-            </div>
-          </section>
-        </article>
-        `;
-      document.querySelector('.scroll-observer').before($article);
-    });
+            </h2>
+            <p>
+              ${post.description}
+            </p>
+          </div>
+        </section>
+      </article>`).join('');
+
+    $observer.insertAdjacentHTML('beforebegin', domStr);
   };
 
   const fetchArticles = async () => {
     try {
       const url = `https://newsapi.org/v2/top-headlines?country=kr&category=${category}&page=${page}&pageSize=${MAX_PAGE}&apiKey=${API_KEY}`;
-      // eslint-disable-next-line no-undef
       const response = await axios.get(url);
-
       render(response.data.articles);
+      console.log('fetch 데이터 중');
     } catch (error) {
-      throw new Error(error, '데이터를 불러올 수 없습니다. 잠시 후 다시 시도해주세요.');
+      throw new Error(error);
     }
   };
 
-  const infiniteScroll = () => {
+  const infiniteScroll = entries => {
+    if (entries[0].intersectingRatio === 1) return;
+    if (!entries[0].isIntersecting) return;
+
     page++;
     fetchArticles();
   };
 
-  document.addEventListener('DOMContentLoaded', async () => {
-    await fetchArticles();
-
+  const startObserving = () => {
     const $observer = new IntersectionObserver(infiniteScroll, { threshold: 0.7 });
     const target = document.querySelector('.scroll-observer');
 
     $observer.observe(target);
+  };
+
+  document.addEventListener('DOMContentLoaded', () => {
+    initialize();
+    startObserving();
   });
 
-  $root.addEventListener('category-change', async e => {
+  $root.addEventListener('category-change', e => {
     page = 1;
-    category = e.detail.id;
+    category = e.detail.id === 'all' ? '' : e.detail.id;
 
-    document.querySelector('.news-list-container').innerHTML = '';
-    await fetchArticles();
+    $root.querySelector('.news-list-container').remove();
+    initialize();
+    startObserving();
   });
 };
 
