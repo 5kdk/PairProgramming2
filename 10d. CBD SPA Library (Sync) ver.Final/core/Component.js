4d3304d6674fe52b5d9ffd676a6200163a10b6b3
Kyu Sung Sohn <rok.ksohn@gmail.com>
2023-03-09 19:20:47 +0900

    refactor: Add library render function comment
    Co-authored-by: Dongkyu Kim <5kdk@users.noreply.github.com>

--- 10d. CBD SPA Library (Sync) ver.Final/core/Component.js
+++ 10d. CBD SPA Library (Sync) ver.Final/core/Component.js
@@ -18,6 +18,13 @@ class Component {
     throw new Error("Component의 서브 클래스는 'render' 메서드를 구현해야 합니다.");
   }
 
+  /**
+   *
+   * @param {string} eType: on which event the callback will be called upon
+   * @param {string} el: the CSS selector of the element that triggers the event
+   * @param {function} callback: the callback function that will be invoked when the event occurs
+   * @returns {void} if no duplicate event exists, new event listener is attached to root
+   */
   addEvent(eType, el, callback) {
     if (eventStorage.some(({ eventType, selector }) => eventType === eType && selector === el)) return;
 
